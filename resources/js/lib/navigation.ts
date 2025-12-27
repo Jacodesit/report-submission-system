cff8706f8cc3f21e814f83eb8603ede0edb917f9
mrf3v3r <jojehterce@gmail.com>
2025-12-27 13:52:53 +0800

    Add reports functionality and enhance user role handling
    - Implemented reports method in ViewController for focal person.
    - Updated HandleInertiaRequests to include detailed user information.
    - Created useAuth hook for better user state management.
    - Defined main navigation paths based on user roles.
    - Added reports page for focal person with breadcrumbs.

---
+++ resources/js/lib/navigation.ts
@@ -0,0 +1,51 @@
+import FieldOfficerNavigationPath from '@/actions/App/Http/Controllers/FieldOfficer/ViewController';
+import FocalPersonNavigationPath from '@/actions/App/Http/Controllers/FocalPerson/ViewController';
+import ProgramHeadNavigationPath from '@/actions/App/Http/Controllers/ProgramHead/ViewController';
+import ProvincialDirectorNavigationPath from '@/actions/App/Http/Controllers/ProvincialDirector/ViewController';
+import { NavItem } from '@/types';
+import { File, LayoutGrid } from 'lucide-react';
+
+export function mainNavigationPath(role: string): NavItem[] {
+    switch (role) {
+        case 'field_officer':
+            return [
+                {
+                    title: 'Dashboard',
+                    href: FieldOfficerNavigationPath.dashboard().url,
+                    icon: LayoutGrid,
+                },
+            ];
+        case 'focal_person':
+            return [
+                {
+                    title: 'Dashboard',
+                    href: FocalPersonNavigationPath.dashboard().url,
+                    icon: LayoutGrid,
+                },
+                {
+                    title: 'Reports',
+                    href: FocalPersonNavigationPath.reports().url,
+                    icon: File,
+                },
+            ];
+        case 'program_head':
+            return [
+                {
+                    title: 'Dashboard',
+                    href: ProgramHeadNavigationPath.dashboard().url,
+                    icon: LayoutGrid,
+                },
+            ];
+
+        case 'provincial_director':
+            return [
+                {
+                    title: 'Dashboard',
+                    href: ProvincialDirectorNavigationPath.dashboard().url,
+                    icon: LayoutGrid,
+                },
+            ];
+        default:
+            return [];
+    }
+}
---
+++ resources/js/pages/focal-person/reports/page.tsx
@@ -0,0 +1,10 @@
+import AppLayout from '@/layouts/app-layout';
+import { breadcrumbs } from '../dashboard/page';
+
+export default function CreateReport() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <div>fsfdsfds</div>
+        </AppLayout>
+    );
+}
